2e18ef47a853c59f04a502a148c9ad482805460d
alvaropeblas <121167838+alvaropeblas@users.noreply.github.com>
2024-06-20 23:48:28 +0200

    muestra el calendario

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,57 @@
-@extends('layouts.app')
-
 @extends('adminlte::page')
- 
-@section('title', 'AdminLTE')
- 
-@section('content\_header')
-    <h1 class="m-0 text-dark">Dashboard</h1>
-@stop
- 
-@section('content')
 
+@section('title', 'Dashboard')
+
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div id="calendar"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
+
+@section('css')
+<style>
+    .months-container {
+        height: 100vh;
+        width: 100%;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        gap: 10px;
+    }
+
+    .calendar-header {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    table {
+        width: 100%;
+        margin-bottom: 2.5%;
+        margin-top: 1.5%;
+    }
+</style>
+@stop
+
+@section('js')
+<script src="https://unpkg.com/js-year-calendar@latest/dist/js-year-calendar.min.js"></script>
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        var calendar = new Calendar('#calendar', {
+            minDate: new Date(),
+            language: 'es',
+            style: 'background',
+        });
+        calendar.render();
+    });
+
+</script>
 @stop
